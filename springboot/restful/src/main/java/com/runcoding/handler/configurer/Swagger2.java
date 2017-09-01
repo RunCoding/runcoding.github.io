f8fc406a388a306f9dbe93f0343f5fa0225d7f50
徐凯 <xukai@xukaideMacBook-Pro.local>
2017-09-01 18:10:57 +0800

    使用Spring boot 构建项目

--- springboot/restful/src/main/java/com/runcoding/handler/configurer/Swagger2.java
+++ springboot/restful/src/main/java/com/runcoding/handler/configurer/Swagger2.java
@@ -1,4 +1,4 @@
-package com.runcoding;
+package com.runcoding.handler.configurer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
